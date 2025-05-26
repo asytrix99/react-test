f3d5059930456bf779e8f427c847aa0bcca43472
Andrew <e1398281@u.nus.edu>
2025-05-27 04:08:04 +0800

    added inline css

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,18 @@
 import React from 'react'
 
 const Header = () => {
+    const headerStyle = {
+        backgroundColor: 'royalblue',
+        textAlign: 'center',
+        width: 300,
+        color: 'white',
+        margin: 'auto',
+        padding: '0px',
+        fontFamily: 'cursive'
+    }
+    
   return (
-    <header>
+    <header style={headerStyle}>
         <h1>Groceries List</h1>
     </header>
   )
